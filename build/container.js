1d75f55c7dc46eef36914d9a4403356f90eaa9b0
Brian Olencki <brian@olenckicom>
2021-01-30 14:20:37 -0500

    Add new build

---
+++ build/container.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const service_container_1 = require("@halliganjs/service-container");
+const container = new service_container_1.Container();
+exports.default = container;
+//# sourceMappingURL=container.js.map
